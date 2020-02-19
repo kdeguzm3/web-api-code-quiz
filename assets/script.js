3817c800a1456dbf1fd78edd93e79fa089688be4
Alex Taylor <altays633@gmail.com>
2020-02-18 19:14:30 -0500

    added variables for page section handlers, created elements, timer, and page state

--- assets/script.js
+++ assets/script.js
@@ -13,3 +13,26 @@
 
     // WHEN the game is over
     // THEN I can save my initials and score
+
+    // handlers for parts of page
+    
+let timeDiv = document.querySelector("#timeDiv");
+let sectionHeading = document.querySelector("#section-heading");
+let sectionContent = document.querySelector("#section-content");
+let buttonDiv = document.querySelector("#button-div");
+
+// handlers for created elements
+let startButton=document.createElement("button");
+
+// handlers for local storage
+let storedUserName = localStorage.getItem("code-quiz-name");
+
+// timer variable
+let secondsLeft = 30;
+
+// variables for page state
+
+let indexState=true;
+let questionState=false;
+let gameOverState=false;
+let highScoresState=true
